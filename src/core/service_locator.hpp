ad60701ad7c798b240858b23c8f379f037a4dbf2
s1mple_xyz <mrzkk07@gmail.com>
2026-08-26 13:47:13 +0530

    basic input manager

---
+++ src/core/service_locator.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+namespace wind::core {
+class ServiceLocator
+{
+public:
+  template <typename T>
+  auto static provide(T& service) WIND_NOEXCEPT->void
+  {
+    instance<T>() = service;
+  }
+
+  template <typename T>
+  auto static provide(T* service) WIND_NOEXCEPT->void
+  {
+    instance<T>() = service;
+  }
+
+
+  template <typename T>
+  static auto get() WIND_NOEXCEPT -> T&
+  {
+    T* service = instance<T>();
+
+    WIND_ASSERT(service && "Service has not been registered");
+
+    return *service;
+  }
+
+private:
+  template <typename T>
+  static auto instance() -> T*&
+  {
+    static T* ptr = nullptr;
+    return ptr;
+  }
+};
+}  // namespace wind::core
